30b2e83a1cee30d4823d6ce2f06dffc4ec2df2f3
henrik <geijstedt@telia.com>
2016-06-04 20:36:29 +0200

    Initial commit

---
+++ PropertyFinder/index.ios.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+AppRegistry,
+StyleSheet,
+Text,
+NavigatorIOS
+} from 'react-native';
+
+
+var SearchPage = require('./SearchPage');
+
+var styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class HelloWorld extends Component {
+  render() {
+    return <Text style={styles.text}>Hello World (Again)</Text>;
+  }
+}
+
+class PropertyFinderApp extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
